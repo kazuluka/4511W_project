f798d9460c995a55e9b7d9319a61f953471cd97e
Patrick Severin <sever408@umn.edu>
2013-05-03 15:22:40 -0500

    Node brought out into a separate class. I'm a hero.

--- src/pkg4511/Main.java
+++ src/pkg4511/Main.java
@@ -39,37 +39,6 @@ import javax.swing.filechooser.*;
 
 public class Main extends JPanel implements ActionListener {
 
-   public class Node {                      //node object
-   public int x = 0;
-   public int y = 0;
-   public int coverage = 0;
-   public boolean occupied = false;
-   public String type = "NULL"; //The type of node -- wall, floor, nothing, camera, possible
-   public String orientation = "NULL"; //This is used for cameras -- the direction they are pointing
-   public Node(int a, int b, String setType) { //constructor
-       x = a;
-       y = b;
-       type = setType;
-   }
-   public void setCoverage(int cov){ //This is used for cameras
-       coverage = cov;
-   }
-   public int getCoverage(){ //This is used for cameras
-       return coverage;
-   }
-   public void setType(String setType){ //This is used for cameras
-       type = setType;
-   }
-   public String getType(){ //This is used for cameras
-       return type;
-   }
-   public void setOri(String direction){ //This is used for cameras
-       orientation = direction;
-   }
-   public String getOri(){ //This is used for cameras
-       return orientation;
-   }
-  }
   Random generator = new Random();
   public int pNodeCount=0;
   public int CameraCount=0;
