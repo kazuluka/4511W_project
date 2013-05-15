c05beb99d4cbeec34562492ccebc9d9ec30ab28c
Jessica McMillan <kemp0158@umn.edu>
2013-05-15 10:41:41 -0500

    Updates for the mesh mapping.

--- src/pkg4511/MeshOption.java
+++ src/pkg4511/MeshOption.java
@@ -9,18 +9,28 @@ package pkg4511;
  * @author Jess
  */
 public class MeshOption {
-    Node startPoint, endPoint;
+    MeshPoint startPoint, endPoint;
     float distance = Integer.MAX_VALUE;
+    Node    sNode, eNode;
     
-    public MeshOption(Node s, Node e){
+    public MeshOption(MeshPoint s, MeshPoint e){
         startPoint = s;
         endPoint = e;
+        sNode = s.currentNode;
+        eNode = e.currentNode;
+        distance = getDistance();
+    }
+    
+    public MeshOption(Node s, MeshPoint e){
+        endPoint = e;
+        sNode = s;
+        eNode = e.currentNode;
         distance = getDistance();
     }
     
     public float getDistance(){
-        int a = startPoint.x-endPoint.x;
-        int b = startPoint.y-endPoint.y;
+        int a = sNode.x-eNode.x;
+        int b = sNode.y-eNode.y;
         if (a==0 && b==0){
             return 0;
         }
--- src/pkg4511/MeshPointEvaluator.java
+++ src/pkg4511/MeshPointEvaluator.java
@@ -12,21 +12,21 @@ import java.util.Arrays;
  * @author Jess
  */
 public class MeshPointEvaluator {
-    //8 cases - example case below
-    // Checking outward corners     x x o
-    // x wall | o floor             x x o
-    // 4 rotations to cover cases   o o o <-BR meshPoint
-    int[][] checkNodesTR = {{0,0,0},{1,1,0},{1,1,0}}; 
-    int[][] checkNodesTL = {{0,0,0},{0,1,1},{0,1,1}};
-    int[][] checkNodesBR = {{1,1,0},{1,1,0},{0,0,0}};
-    int[][] checkNodesBL = {{0,1,1},{0,1,1},{0,0,0}};
-    int[][] checkNodesM1 = {{1,1,1},{1,0,0},{1,0,0}};
-    int[][] checkNodesM2 = {{1,1,1},{0,0,1},{0,0,1}};
-    int[][] checkNodesM3 = {{1,0,0},{1,0,0},{1,1,1}};
-    int[][] checkNodesM4 = {{0,0,1},{0,0,1},{1,1,1}};
+    //8 cases different 2x2 cases for determining if a node is a corner.
+    // Checking outward corners     
+    // x wall | o floor              x o
+    // 4 rotations to cover cases    o o <-BR createMeshPoint
+    int[][] checkNodesTR = {{0,0},{1,0}}; 
+    int[][] checkNodesTL = {{0,0},{0,1}};
+    int[][] checkNodesBR = {{1,0},{0,0}};
+    int[][] checkNodesBL = {{0,1},{0,0}};
+    int[][] checkNodesM1 = {{1,1},{1,0}};
+    int[][] checkNodesM2 = {{1,1},{0,1}};
+    int[][] checkNodesM3 = {{1,0},{1,1}};
+    int[][] checkNodesM4 = {{0,1},{1,1}};
     
-    Node[][] toCheck = new Node[3][3];
-    int[][] intToCheck = new int[3][3];
+    Node[][] toCheck = new Node[2][2];
+    int[][] intToCheck = new int[2][2];
     MeshPoint meshPoint = null;    
     
     public MeshPointEvaluator(){}
@@ -34,13 +34,13 @@ public class MeshPointEvaluator {
     public MeshPointEvaluator(Node[][] toCheck){    
         this.toCheck = toCheck;
         translateToCheck();
-        meshPoint();
+        createMeshPoint();
     }
 
     public void translateToCheck(){
         NodeType toCheckType;
-        for(int i=0; i<3;i++){
-            for(int k = 0; k<3;k++){
+        for(int i=0; i<2;i++){
+            for(int k = 0; k<2;k++){
                 toCheckType = toCheck[i][k].type;
                 if((toCheckType==NodeType.WALL) || (toCheckType == NodeType.DOOR)){
                     intToCheck[i][k] = 1;
@@ -53,10 +53,10 @@ public class MeshPointEvaluator {
         }
     }
     
-    public void meshPoint(){
+    public void createMeshPoint(){
         // <editor-fold defaultstate="collapsed" desc="Outside Corners">        
         if(Arrays.deepEquals(checkNodesTR, intToCheck)){
-            meshPoint = new MeshPoint(toCheck[0][2], checkNodesTR, "TR");
+            meshPoint = new MeshPoint(toCheck[0][1], checkNodesTR, "TR");
             //System.out.println("TR");
             return;
         }
@@ -66,12 +66,12 @@ public class MeshPointEvaluator {
             return;
         }
         if(Arrays.deepEquals(checkNodesBL, intToCheck)){
-            meshPoint = new MeshPoint(toCheck[2][0], checkNodesBL, "BL");
+            meshPoint = new MeshPoint(toCheck[1][0], checkNodesBL, "BL");
             //System.out.println("BL");
             return;
         }        
         if(Arrays.deepEquals(checkNodesBR, intToCheck)){
-            meshPoint = new MeshPoint(toCheck[2][2], checkNodesBR, "BL");
+            meshPoint = new MeshPoint(toCheck[1][1], checkNodesBR, "BR");
             //System.out.println("BR");
             return;
         }
@@ -84,17 +84,17 @@ public class MeshPointEvaluator {
             return;
         }        
         if(Arrays.deepEquals(checkNodesM2, intToCheck)){
-            meshPoint = new MeshPoint(toCheck[1][1], checkNodesM2, "M2");
+            meshPoint = new MeshPoint(toCheck[1][0], checkNodesM2, "M2");
             //System.out.println("M2");
             return;
         }     
         if(Arrays.deepEquals(checkNodesM3, intToCheck)){            
-            meshPoint = new MeshPoint(toCheck[1][1], checkNodesM3, "M3");
+            meshPoint = new MeshPoint(toCheck[0][1], checkNodesM3, "M3");
             //System.out.println("M3");
             return;
         }     
         if(Arrays.deepEquals(checkNodesM4, intToCheck)){
-            meshPoint = new MeshPoint(toCheck[1][1], checkNodesM4, "M4");
+            meshPoint = new MeshPoint(toCheck[0][0], checkNodesM4, "M4");
             //System.out.println("M4");
             return;
         }
@@ -105,7 +105,7 @@ public class MeshPointEvaluator {
         if(meshPoint==null){
             return false;
         }
-        //System.out.println("Adding Node: ("+meshPoint.currentNode.x+", "+meshPoint.currentNode.y+")");
+        //System.out.println("Adding Node: ("+createMeshPoint.currentNode.x+", "+createMeshPoint.currentNode.y+")");
         return true;
     }
     
